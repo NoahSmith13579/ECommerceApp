bfc883fda161cab9e2161d70a26225ff19fd61af
Noah Smith <noahbsmith13579@gmail.com>
2023-06-14 11:50:51 -0500

    Deployment and address model change

--- ShoppingApp/Models/Address.cs
+++ ShoppingApp/Models/Address.cs
@@ -14,10 +14,10 @@ namespace ShoppingApp.Models
         [RegularExpression(@"^[a-zA-Z0-9 ]+$", ErrorMessage = "Street2 may contain only alphanumeric characters")]
         public string? Street2 { get; set; }
         [Required]
-        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "City may only contain alphabetical characters")]
+        [RegularExpression(@"^([a-zA-Z\u0080-\u024F]+(?:. |-| |'))*[a-zA-Z\u0080-\u024F]*$", ErrorMessage = "City may not contain special characters")]
         public string City { get; set; }
         [Required]
-        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Country may only contain alphabetical characters")]
+        [RegularExpression(@"^([a-zA-Z\u0080-\u024F]+(?:. |-| |'))*[a-zA-Z\u0080-\u024F]*$", ErrorMessage = "Country may only contain alphabetical characters")]
         public string Country { get; set; }
         [Required]
 
